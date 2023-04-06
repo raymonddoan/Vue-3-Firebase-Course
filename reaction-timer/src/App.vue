c2813ed104cb6ab6626fab778c064d119fce4611
raymondDoanAtPropertyMe <raymonddoan@propertyme.com>
2023-04-06 11:52:42 +1000

    showing results component

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -2,8 +2,7 @@
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction Time: {{ score }} ms</p>
-  <Results />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script lang="ts">
--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-
+    <p>Reaction Time: {{ score }} ms</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({})
+export default defineComponent({
+  props: ['score']
+})
 </script>
 
 <style scoped>
