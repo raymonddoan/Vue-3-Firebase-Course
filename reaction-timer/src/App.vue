e613956d167be7811fa2fec531b533c2851a540e
raymondDoanAtPropertyMe <raymonddoan@propertyme.com>
2023-04-06 10:20:29 +1000

    New reaction timer project

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Reaction Timer</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
