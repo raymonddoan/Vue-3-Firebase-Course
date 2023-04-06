c7a5fa5d2b558b10b3d9a6f25b3323a3e2e3e852
raymondDoanAtPropertyMe <raymonddoan@propertyme.com>
2023-04-06 11:57:59 +1000

    finishing touches

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <p>Reaction Time: {{ score }} ms</p>
+    <p>Rank: {{ rank }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  props: ['score']
-})
+  props: ["score"],
+  data() {
+    return {
+      rank: "",
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Finger Bangerz";
+    } else if (this.score < 400) {
+      this.rank = "Speedy Gonzalez";
+    } else {
+      this.rank = "Turtle wins the race";
+    }
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
