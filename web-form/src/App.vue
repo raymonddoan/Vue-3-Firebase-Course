06be50d9a22c9ac021481122bb373247ed3c81bf
raymondDoanAtPropertyMe <raymonddoan@propertyme.com>
2023-04-06 13:29:18 +1000

    add signupForm

--- web-form/src/App.vue
+++ web-form/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>Hello World</h1>
+  <SignUpForm/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SignUpForm from "./components/SignupForm.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { SignUpForm },
 });
 </script>
 
@@ -19,5 +20,9 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
