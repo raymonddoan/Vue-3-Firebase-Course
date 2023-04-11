816620dbecf114d59191213930e6a7d626709470
Ray <122244100+raymondDoanAtPropertyMe@users.noreply.github.com>
2023-04-11 12:23:25 +1000

    folder structure

--- ninja-jobs/src/router/index.ts
+++ ninja-jobs/src/router/index.ts
@@ -1,17 +1,27 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import AboutViewVue from "../views/AboutView.vue";
 import HomeView from "../views/HomeView.vue";
+import JobsView from "../views/jobs/JobsView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
   },
   {
     path: "/about",
-    name: "about",
+    name: "About",
     component: AboutViewVue,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: JobsView,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ ninja-jobs/src/views/jobs/JobsView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      jobs: [
+        {
+          title: "Software Developer",
+          id: 1,
+          details: "Developing the software",
+        },
+        {
+          title: "Software Designer",
+          id: 2,
+          details: "Designing the software",
+        },
+        { title: "Engineering Manager", id: 3, details: "Managing the team" },
+      ],
+    };
+  },
+};
+</script>
